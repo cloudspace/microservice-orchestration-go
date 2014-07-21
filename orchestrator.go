b26b9d1f57328d8aed46342c5846ae79b6cefb1e
Todd Sampson <tsampson@cloudspace.com>
2014-07-21 12:41:53 -0400

    Justin got the basics working.  Needs lots of love - removing prints, drying code, json input/output - but it is moving in the right direction.

--- orchestrator.go
+++ orchestrator.go
@@ -11,6 +11,7 @@ type Service struct {
     Id string
     DockerImage string
     Inputs map[string]map[string]string
+    Outputs []string
 }
 
 // go run orchestrator.go "[{\"id\":\"1\",\"dockerImage\":\"cloudspace/url-lengthener-go\",\"inputs\":{\"url\":{\"service\":\"input\",\"key\":\"url\"}}},{\"id\":\"2\",\"dockerImage\":\"cloudspace/utm-stripper-go\",\"inputs\":{\"url\":{\"service\":\"1\",\"key\":\"url\"}}}]" "{\"url\":\"http://t.co/wnpJFiP7ls\"}"
@@ -52,59 +53,63 @@ func main() {
     fmt.Printf("%+v", options)
     fmt.Println("\n")
 
-    var user_input map[string]map[string]string
-
     // prelaunch
-    var outputs map[string]map[string]chan string
+    outputs := make(map[string]map[string]chan string)
 
     // outputs["0"] := map[string]chan
 
+    user_input := make(map[string]chan string)
     for key, input := range options {
         chn := make(chan string, 1)
         chn <- input
-        outputs["0"][key] = chn
+        user_input[key] = chn
     }
- 
+    outputs["input"] = user_input
+
     /////////////////////////////////////////////////////
     fmt.Println("RUNNING...")
 
-    output := user_input
-    for index, service := range flow {
-        var input map[string]chan string
-        for key,value := range service.Inputs {
+    for _, service := range flow {
+        input := make(map[string]chan string)
+        for key, value := range service.Inputs {
             input[key] = outputs[value["service"]][value["key"]]
         }
 
-        outputs[service.Id] = runService(input, service.DockerImage)
+        outputs[service.Id] = runService(input, service)
 
-        output = runService(output, service.DockerImage)
+        // output = runService(output, service.DockerImage)
         fmt.Println("ID:",service.Id)
     }
+    fmt.Println(<- outputs["2"]["url"])
 
-
-    // c := make(chan string)
-    
-    // out := runService(c, flow[0].DockerImage)
-    
-    // fmt.Println("HELLO:",flow[0].Inputs["url"]["key"], "\n")
-
-    // c <- flow[0].Inputs["url"]["key"]
-    // fmt.Printf("%s", <-out)
 }
 
-func runService(inputs map[string]chan string, dockerImage string) <-chan string {
-    result := make(chan string)
+func runService(inputs map[string]chan string, service Service) map[string]chan string {
+    result := make(map[string]chan string)
+    for _, key := range service.Outputs {
+      result[key] = make(chan string, 1)
+    }
     go func() {
-        args := <-arg1
-        out, err := exec.Command("docker", "run", dockerImage, args).Output()
-    
+        fmt.Println(service.Id)
+        args := <-inputs["url"]
+        fmt.Println(service.Id, args)
+        out, err := exec.Command("docker", "run", service.DockerImage, args).Output()
+
+        fmt.Println(out)
+
         if err != nil {
             fmt.Println("ERROR:")
             fmt.Println(err)
             return
         }
-
-        result <- fmt.Sprintf("%s", out)
+        var dat map[string]string
+        if err := json.Unmarshal([]byte(out), &dat); err != nil {
+          panic(err)
+        }
+        for key, value := range dat {
+          fmt.Println(service.Id, key, value)
+          result[key] <- value
+        }
     }()
     return result
 }
